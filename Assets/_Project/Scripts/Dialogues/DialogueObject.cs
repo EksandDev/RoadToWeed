9f0f14f3e44a817caa9e3491a40d2ec5800fba60
EksandDev <153660809+EksandDev@users.noreply.github.com>
2025-04-13 03:02:39 +0400

    Another some fixes

--- Assets/_Project/Scripts/Dialogues/DialogueObject.cs
+++ Assets/_Project/Scripts/Dialogues/DialogueObject.cs
@@ -4,8 +4,6 @@ namespace _Project.Scripts.Dialogues
 {
     public abstract class DialogueObject : MonoBehaviour
     {
-        [SerializeField] private Transform _pointToRotatePlayer;
-
         public abstract void Initialize(DialogueDependencies dialogueDependencies);
         public abstract void StartDialogue();
     }
